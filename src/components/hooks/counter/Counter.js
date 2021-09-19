16590cd42c470bc8a9c2ab77b6e0994da72a8a33
saquib156 <saquibjamal.jamal156@gmail.com>
2021-09-19 17:10:00 +0530

    update counter

--- src/components/hooks/counter/Counter.js
+++ src/components/hooks/counter/Counter.js
@@ -1,28 +1,29 @@
 import React, {useState, useEffect} from "react";
 import './Counter.css';
 
-const Counter = (props) => {
+const Counter = () => {
     const [count, setCount] = useState(0);
+    const [color, setColor] = useState("");
 
     useEffect(() => {
         if(count===0){
-            document.getElementById("countVal").style.color = '#666666';
+            setColor('grey');
         } else if(count < 0){
-            document.getElementById("countVal").style.color = 'red';
-        } else if(count > 0){
-            document.getElementById("countVal").style.color = 'green';
+            setColor('red');
+        } else {
+            setColor('green');
         }
     }, [count]);
 
     return (
         <>
-            <div class="counter-box">
-                <h1 class="head">Counter Using Hooks</h1>
-                <h2 class="head" id="countVal">{count}</h2>
-                <div class="btn-box">
-                    <button class="btn" onClick={() => setCount(count-1) }>Decrease</button>
-                    <button class="btn" onClick={() => setCount(0) }>Reset</button>
-                    <button class="btn" onClick={() => setCount(count+1) }>Increase</button>
+            <div className="counter-box">
+                <h1 className="head">Counter Using Hooks</h1>
+                <h2 className={color} >{count}</h2>
+                <div className="btn-box">
+                    <button className="btn" onClick={() => setCount(count-1) }>Decrease</button>
+                    <button className="btn" onClick={() => setCount(0) }>Reset</button>
+                    <button className="btn" onClick={() => setCount(count+1) }>Increase</button>
                 </div>
             </div>
         </>
